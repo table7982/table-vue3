e2fbc4bf582d104e6afdd1e6b2acc74da2d912af
table7982 <2381745984@qq.com>
2024-11-21 01:42:19 +0800

    path bug fix

--- src/components/pages/work/WorkIndex.vue
+++ src/components/pages/work/WorkIndex.vue
@@ -13,10 +13,10 @@ import Head from './Head.vue';
 import workBox from './workBox.vue';
 
 const workList = [
-  { title: '自画像', discribe: '我的自画像', imgLink: '../../../../table-vue3/img/workImg/selfImg.png' },
-  { title: '服装设计', discribe: '一个男装设计', imgLink: '../../../../table-vue3/img/workImg/customImgBoy.png' },
-  { title: '服装设计', discribe: '一个女装设计', imgLink: '../../../../table-vue3/img/workImg/customImgGirl.png' },
-  { title: '素描', discribe: '一副小素描', imgLink: '../../../../table-vue3/img/workImg/Drawing1.png' },
+  { title: '自画像', discribe: '我的自画像', imgLink: '../../../../img/workImg/selfImg.png' },
+  { title: '服装设计', discribe: '一个男装设计', imgLink: '../../../../img/workImg/customImgBoy.png' },
+  { title: '服装设计', discribe: '一个女装设计', imgLink: '../../../../img/workImg/customImgGirl.png' },
+  { title: '素描', discribe: '一副小素描', imgLink: '../../../../img/workImg/Drawing1.png' },
 ]
 </script>
 
