4851974cde0b419f260e029c434edd5cf9824d68
table7982 <2381745984@qq.com>
2025-01-11 09:56:10 +0800

    diary str_num add

--- src/components/pages/diary/diaryHooks/useDiaryData.ts
+++ src/components/pages/diary/diaryHooks/useDiaryData.ts
@@ -5,7 +5,7 @@ export default function () {
 
   const DiaryList: any = [
     { diary_id: 'id03', diary_title: '写点什么', img_url: diary_img_url_id03, diary_time: '2024年 12月 2日 星期一', diary_discribe: '为什么要做这个网站？', route_link: '/diaryitem/id03', str_num: 1093 },
-    { diary_id: 'id02', diary_title: '苏州一日行', img_url: diary_img_url_id02, diary_time: '2024年 11月 23日 星期六', diary_discribe: '特种兵之速通苏州，“月落乌啼霜满天~”.', route_link: '/diaryitem/id02', str_num: 0 },
+    { diary_id: 'id02', diary_title: '苏州一日行', img_url: diary_img_url_id02, diary_time: '2024年 11月 23日 星期六', diary_discribe: '特种兵之速通苏州，“月落乌啼霜满天~”.', route_link: '/diaryitem/id02', str_num: 2866 },
     { diary_id: 'id01', diary_title: '关于设计', img_url: diary_img_url_id01, diary_time: '2025年 7月 1日 星期二', diary_discribe: '给秋天一个约定，请春天来见证。', route_link: '/diaryitem/id01', str_num: 0 },
   ]
 
